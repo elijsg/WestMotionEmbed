edfcc438517dd77dd47965d7cb30a7a61650f9c9
elijsg <elijahsgregory@gmail.com>
2023-07-25 10:39:18 -0700

    Change Colors

--- src/components/bubbles/GuestBubble.tsx
+++ src/components/bubbles/GuestBubble.tsx
@@ -10,7 +10,7 @@ type Props = {
   textColor?: string
 }
 
-const defaultBackgroundColor = '#3B81F6'
+const defaultBackgroundColor = '#BD1583'
 const defaultTextColor = '#ffffff'
 
 Marked.setOptions({ isNoP: true })
